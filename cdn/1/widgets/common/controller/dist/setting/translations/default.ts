426e687612e17cc51517b9b1f6d61afbc3a08c03
shawnmgoulet <sgoulet@esri.com>
2025-02-26 19:29:00 +0000

    Deploying to gh-pages from @ shawnmgoulet/experience-builder-devops-example@dba7c77d9639135287f506fed33d1c10537724e6 🚀

--- cdn/1/widgets/common/controller/dist/setting/translations/default.ts
+++ cdn/1/widgets/common/controller/dist/setting/translations/default.ts
@@ -7,12 +7,16 @@ export default {
   showIconLabel: 'Label',
   iconSizeOverride: 'Size',
   iconInterval: 'Spacing',
-  textFormatOverride: 'Text',
-  iconBackgroundOverride: 'Icon',
+  advancedIconStyle: 'Icon style',
+  advancedIconColor: 'Icon color',
   openStart: 'Open widgets on loading',
   widgetsSelected: '{widgetNumber} widgets selected',
   appearanceTip: 'Select the widgets inside the Widget Controller, and customize their icons, labels, or styles to change their appearances.',
+  appearanceTipExpress: 'Customize widget icon styles within the Widget Controller to change their appearances.',
   widgetPanelArrangment: 'Widget panel arrangement',
   panelFloating: 'Floating',
-  positionAndSize: 'Position & size'
+  indicator: 'Indicator',
+  messageAction_openWidget: 'Open widget',
+  overflownStyle: 'Overflown style',
+  popupWindow: 'Popup window'
 }
